e4a8a75ca7fb6cb66420ca30c0d2004a2a61cf5b
ana? <anajariyatunkhasanah1@gmail.com>
2026-03-30 10:16:33 +0700

    add new files

---
+++ public/service-worker-backup.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "portal-aplikasi-v1";
+const ASSETS = [
+  "/user/dashboard",
+  "/manifest.json",
+  "/icon/icon-192.png",
+  "/icon/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
